bb357388c1d68c01e576846b810dd8f150f513b2
dauztas <ovid.pp@yahoo.com>
2022-10-26 22:34:32 +0300

    task1Completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,21 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+document.getElementById("output").style.marginLeft = "200px";
+document.getElementById("output").style.marginRight = "200px";
+document.getElementById("output").style.padding = "20px";
+document.getElementById("output").style.backgroundColor = "black";
+document.getElementById("output").style.color = "white";
+document.getElementById("output").style.border = "2px solid black";
+const form = document.querySelector("form");
+function convertKg(e) {
+  e.preventDefault();
+  let input = document.getElementById("search").value;
+  let allInOne = [
+    "Lbs: " + input * 2.2046,
+    "Grams: " + input / 0.001,
+    "Oz: " + input / 0.0283495231
+  ];
+  document.getElementById("output").innerHTML = allInOne.join("</br>");
+}
+form.addEventListener("submit", convertKg);
