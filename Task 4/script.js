6495cd955472740e925f2fd08936f1c041f3c977
dauztas <ovid.pp@yahoo.com>
2022-10-27 22:37:50 +0300

    task4 partly completed

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,34 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+fetch(ENDPOINT)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    displayData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+let myOutput = document.getElementById("output");
+myOutput.style.marginLeft = "20px";
+myOutput.style.border = "2px solid black";
+myOutput.style.backgroundColor = "beige";
+
+function displayData(data) {
+  for (let i = 0; i < data.length; i++) {
+    myOutput.innerHTML +=
+      "Brand: " +
+      data[i].brand +
+      "</br> " +
+      "Model: " +
+      data[i].models +
+      "</br>";
+
+    console.log(myOutput);
+  }
+}
